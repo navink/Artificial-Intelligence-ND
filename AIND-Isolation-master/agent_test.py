1f05416ede970442a8c93e2917a0027fc5a7c3e5
NavinK <navinkolambkar@Navins-MacBook-Pro.local>
2017-12-07 07:28:06 -0800

    AI Projects
    Artificial Intelligence Udacity NanoDegree Part I Projects

---
+++ AIND-Isolation-master/agent_test.py
@@ -0,0 +1,33 @@
+"""This file is provided as a starting template for writing your own unit
+tests to run and debug your minimax and alphabeta agents locally.  The test
+cases used by the project assistant are not public.
+"""
+
+import unittest
+
+import isolation
+import game_agent
+
+from importlib import reload
+from sample_players import (RandomPlayer, open_move_score,
+                            improved_score, center_score)
+from game_agent import (MinimaxPlayer, AlphaBetaPlayer, custom_score,
+                        custom_score_2, custom_score_3)
+
+class IsolationTest(unittest.TestCase):
+    """Unit tests for isolation agents"""
+
+    def setUp(self):
+        reload(game_agent)
+        self.player1 = "Player1"
+        self.player2 = "Player2"
+        self.game = isolation.Board(self.player1, self.player2)
+
+    def test_minimax(self):
+        self.game._board_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 56, 57]
+        self.player1 = AlphaBetaPlayer(2, score_fn=open_move_score)
+        self.player2 = AlphaBetaPlayer(2, score_fn=open_move_score)
+        move = self.player1.alphabeta(self.game, 2)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ AIND-Recognizer-master/asl_recognizer/my_model_selectors.py
@@ -0,0 +1,175 @@
+import math
+import statistics
+import warnings
+
+import numpy as np
+from hmmlearn.hmm import GaussianHMM
+from sklearn.model_selection import KFold
+from asl_utils import combine_sequences
+
+
+class ModelSelector(object):
+    '''
+    base class for model selection (strategy design pattern)
+    '''
+
+    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
+                 n_constant=3,
+                 min_n_components=2, max_n_components=10,
+                 random_state=14, verbose=False):
+        self.words = all_word_sequences
+        self.hwords = all_word_Xlengths
+        self.sequences = all_word_sequences[this_word]
+        self.X, self.lengths = all_word_Xlengths[this_word]
+        self.this_word = this_word
+        self.n_constant = n_constant
+        self.min_n_components = min_n_components
+        self.max_n_components = max_n_components
+        self.random_state = random_state
+        self.verbose = verbose
+
+    def select(self):
+        raise NotImplementedError
+
+    def base_model(self, num_states):
+        # with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        try:
+            hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            if self.verbose:
+                print("model created for {} with {} states".format(self.this_word, num_states))
+            return hmm_model
+        except:
+            if self.verbose:
+                print("failure on {} with {} states".format(self.this_word, num_states))
+            return None
+
+
+class SelectorConstant(ModelSelector):
+    """ select the model with value self.n_constant
+
+    """
+
+    def select(self):
+        """ select based on n_constant value
+
+        :return: GaussianHMM object
+        """
+        best_num_components = self.n_constant
+        return self.base_model(best_num_components)
+
+
+class SelectorBIC(ModelSelector):
+    """ select the model with the lowest Bayesian Information Criterion(BIC) score
+
+    http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+    Bayesian information criteria: BIC = -2 * logL + p * logN
+    """
+
+    def select(self):
+        """ select the best model for self.this_word based on
+        BIC score for n between self.min_n_components and self.max_n_components
+
+        :return: GaussianHMM object
+        """
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        best_bic = float('inf')
+        best_model = None
+        try:
+            for n_components in range(self.min_n_components, self.max_n_components + 1):
+                model = self.base_model(n_components)
+                logL = model.score(self.X, self.lengths)
+                p = (n_components * n_components + 2 * model.n_features * n_components - 1)
+                bic = -2.0 * logL + p * np.log(len(self.X))
+
+                if bic < best_bic:
+                    best_bic = bic
+                    best_model = model
+
+            return best_model
+        except:
+            return self.base_model(self.n_constant)
+
+
+class SelectorDIC(ModelSelector):
+    ''' select best model based on Discriminative Information Criterion
+
+    Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
+    Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
+    https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
+    DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+    '''
+
+    def select(self):
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+        best_dic = float('-inf')
+        best_model = None
+        try:
+            for n_components in range(self.min_n_components, self.max_n_components + 1):
+                model = self.base_model(n_components)
+                scores = list()
+                for word in self.words:
+                    if word is not self.this_word:
+                        X, lengths = self.hwords[word]
+                        scores.append(model.score(X, lengths))
+
+                dic = model.score(self.X, self.lengths) - np.mean(scores)
+                if dic > best_dic:
+                    best_dic = dic
+                    best_model = model
+
+            return best_model
+        except:
+            return self.base_model(self.n_constant)
+
+
+class SelectorCV(ModelSelector):
+    ''' select best model based on average log Likelihood of cross-validation folds
+    '''
+
+    def select(self):
+        """ select the best model for self.this_word based on
+        CV score for n between self.min_n_components and self.max_n_components
+        It is based on log likehood
+        :return: GaussianHMM object
+        """
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        best_score = float('-inf')
+        best_model = None
+        n_splits = min(len(self.sequences), 3)
+
+        for n_components in range(self.min_n_components, self.max_n_components + 1):
+            count = 0
+            scores = list()
+            try:
+                if n_splits > 1:
+                    split_method = KFold(n_splits=n_splits)
+                    for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                        x_train, train_lengths = combine_sequences(cv_train_idx, self.sequences)
+                        x_test, test_lengths = combine_sequences(cv_test_idx, self.sequences)
+                        model = GaussianHMM(n_components=n_components, covariance_type="diag", n_iter=1000,
+                                            random_state=self.random_state, verbose=False).fit(x_train,
+                                                                                               train_lengths)
+                        scores.append(model.score(x_test, test_lengths))
+                        count += 1
+                else:
+                    best_model = self.base_model(self.n_constant)
+                    continue
+
+                if count > 0:
+                    avg_score = np.mean(scores)/count
+                    if avg_score > best_score:
+                        best_score = avg_score
+                        best_model = self.base_model(n_components)
+
+            except:
+                pass
+
+        if best_model is None:
+            best_model = self.base_model(self.n_constant)
+
+        return best_model
